c65bfa7fa35cc5b619d09ba6af3ea83735ecbf77
Bikash9841 <bikashojha904@gmail.com>
2024-06-14 19:53:15 +0545

    New assignment added

---
+++ MIT_Problem_Set/Readme.md
@@ -0,0 +1,14 @@
+# Python Assignment : MIT Problem Set 4
+
+## Objectives:
+
+- To practice thinking about the problem in a recursive fashion.
+- To think problems in terms of classes, their attributes and methods for manipulating them.
+
+## Tasks:
+
+1. Permutations of a string
+2. Cipher Like Caesar
+3. Substitution Cipher
+
+Note: Details on each tasks is provided in the `pdf` inside `docs` folder.
---
+++ MIT_Problem_Set/src/ps4a.py
@@ -0,0 +1,58 @@
+# Problem Set 4A -------------> Permutations of a String
+# Name: <Rituram Ojha>
+# Collaborators:
+# Time Spent: x:xx
+
+
+def get_permutations(sequence):
+    '''
+    Enumerate all permutations of a given string
+
+    sequence (string): an arbitrary string to permute. Assume that it is a
+    non-empty string.  
+
+    You MUST use recursion for this part. Non-recursive solutions will not be
+    accepted.
+
+    Returns: a list of all permutations of sequence
+
+    Example:
+    >>> get_permutations('abc')
+    ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+
+    Note: depending on your implementation, you may return the permutations in
+    a different order than what is listed here.
+    '''
+    if len(sequence) == 1:
+        return [sequence]
+
+    first_char = sequence[0]
+    remaining_permutations = get_permutations(sequence[1:])
+
+    # This will store all the permutations including the first character
+    permutations = []
+
+    # For each permutation of the remaining characters,
+    # insert the first character into every possible position
+    for perm in remaining_permutations:
+        for i in range(len(perm) + 1):
+            # Insert the first character at position i
+            new_permutation = perm[:i] + first_char + perm[i:]
+            permutations.append(new_permutation)
+
+    return permutations
+
+
+if __name__ == '__main__':
+    #    #EXAMPLE
+    #    example_input = 'abc'
+    #    print('Input:', example_input)
+    #    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    #    print('Actual Output:', get_permutations(example_input))
+
+    #    # Put three example test cases here (for your sanity, limit your inputs
+    #    to be three characters or fewer as you will have n! permutations for a
+    #    sequence of length n)
+
+    perm = get_permutations('abcd')
+    print(perm)
